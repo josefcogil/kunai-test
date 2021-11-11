a2c926f1bbfc532a51c0d424915c096d11b4cb6b
Jose Gil <josefcogil27@gmail.com>
2021-11-11 11:44:56 -0400

    feat: add document form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Container, Card, Row, Col } from 'react-bootstrap';
+import DocumentForm from './components/DocumentForm';
+
 const App = () => {
   return (
-    <h1>App</h1>
+    <Container className="py-4">
+      <Card>
+        <Card.Header>
+          <h5>Documentación general convocatoria</h5>
+        </Card.Header>
+        <Card.Body>
+          <Row>
+            <Col>
+              <DocumentForm />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Container>
   );
-}
+};
 
 export default App;
