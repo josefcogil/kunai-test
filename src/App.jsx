fb51bc5e840f002e0b9ffd016d2f005be06ab6fd
Jose Gil <josefcogil27@gmail.com>
2021-11-11 12:24:44 -0400

    feat: add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { Container, Card, Row, Col } from 'react-bootstrap';
 import DocumentForm from './components/DocumentForm';
+import DocumentTable from './components/DocumentTable';
 
 const App = () => {
   return (
     <Container className="py-4">
       <Card>
         <Card.Header>
-          <h5>Documentación general convocatoria</h5>
+          <h5>
+            <b>Documentación general convocatoria</b>
+          </h5>
         </Card.Header>
         <Card.Body>
           <Row>
             <Col>
               <DocumentForm />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Card.Subtitle>
+                <h5>Documentación generales asociados</h5>
+              </Card.Subtitle>
+              <DocumentTable />
             </Col>
           </Row>
         </Card.Body>
